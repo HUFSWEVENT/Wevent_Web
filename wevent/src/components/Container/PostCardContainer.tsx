56a5ee410bcd0072d09e943342abe456eb7e4f86
Lee Da Eun <127325293+daeunleeeee@users.noreply.github.com>
2023-11-04 04:14:23 +0900

    Update PostCardContainer.tsx

--- wevent/src/components/Container/PostCardContainer.tsx
+++ wevent/src/components/Container/PostCardContainer.tsx
@@ -7,8 +7,8 @@ interface RankCardContainerProps {
 
 const RankCardContainerDiv = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr); /* 4열로 설정 */
-  grid-template-rows: repeat(4, 1fr); /* 4열로 설정 */
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr); 
   gap: 3rem;
   margin: auto;
 `;
