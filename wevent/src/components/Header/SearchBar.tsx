819d1d9f01fde31810fe8f7ed777af0026ff649e
daeunleeeee <lde7953@gmail.com>
2023-11-25 02:03:27 +0900

    Feat: SearchBar 기능 추가

---
+++ wevent/src/components/Header/SearchBar.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+  customStyles?: {
+    searchWrapper?: any;
+  };
+}
+
+const SearchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 5rem;
+  border: 1px solid #ccc;
+  border-radius: 3rem;
+  padding: 1rem;
+  margin-right: 4rem;
+  height: 1rem;
+  background-color: white;
+`;
+
+const SearchInput = styled.input`
+  font-size: 1rem;
+  border: none;
+  outline: none;
+  width: 35rem;
+  height: 1.2rem;
+  margin-left: 1rem;
+`;
+
+const SearchIcon = styled.img`
+  width: 1rem;
+  height: 1rem;
+  border: none;
+  cursor: pointer;
+  flex-shrink: 0;
+`;
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (searchTerm.trim() !== '') {
+      onSearch(searchTerm);
+      navigate(`/search/${searchTerm}`);
+    }
+  };
+
+  const handleKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ): void => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <SearchWrapper>
+        <SearchInput
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+          placeholder="협찬하고 싶은 행사를 찾아보세요!"
+        />
+        <SearchIcon
+          src="Images/search-icon.png"
+          alt="searchicon"
+          onClick={handleSearch}
+        />
+      </SearchWrapper>
+    </div>
+  );
+}
