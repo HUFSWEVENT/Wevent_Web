6f90ebe42dd251695d57d0575bcd4e88fa6dbb80
daeunleeeee <lde7953@gmail.com>
2023-11-24 23:56:42 +0900

    Chore: 텍스트 간격 좁히기

--- wevent/src/components/card/PostCard.tsx
+++ wevent/src/components/card/PostCard.tsx
@@ -20,6 +20,7 @@ const Image = styled.img`
   height: 10rem;
   border-radius: 8px;
   cursor: pointer;
+  margin-bottom: -5px;
 `;
 
 const TextContainer = styled.div`
@@ -30,21 +31,21 @@ const Text1 = styled.p`
   font-size: 16px;
   color: #333;
   text-align: right;
-  margin-bottom: 8px;
+  margin-bottom: -10px;
 `;
 
 const Text2 = styled.p`
   font-size: 20px;
   font-weight: 3px;
   color: #666;
-  margin-bottom: 8px;
+  margin-bottom: -10px;
   margin-top: -15px;
 `;
 
 const Text3 = styled.p`
   font-size: 14px;
   color: #666;
-  margin-bottom: 8px;
+  margin-bottom: -10px;
 `;
 
 const Text4 = styled.p`
