a3e3c188ef3ab630594a32d10b7f3d6d20102637
yeram-lim <yeramyeye@gmail.com>
2023-11-26 12:21:13 +0900

    ⚡️[IMPROVE] button component

---
+++ wevent/src/components/button/Button.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import theme from 'styles/theme';
+
+interface BlueButtonProps {
+  children: ReactNode;
+  color?: 'blue' | 'gray';
+  width?: string;
+}
+
+type buttonStyleType = {
+  color: 'blue' | 'gray';
+  width: string;
+};
+
+const StyledButton = styled.button<buttonStyleType>`
+  border-radius: 20px;
+  color: white;
+  background-color: ${(props) =>
+    props.color === 'blue'
+      ? theme.colors['main-color']
+      : props.color === 'gray'
+      ? '#ADB3BA'
+      : props.color};
+  padding: 10px;
+  width: ${(props) => props.width};
+  height: 2.6rem;
+  font-weight: bold;
+  font-size: 1rem;
+  border: none;
+  cursor: pointer;
+`;
+
+export default function Button({
+  children,
+  color = 'blue',
+  width = '8rem',
+}: BlueButtonProps) {
+  return (
+    <StyledButton color={color} width={width}>
+      {children}
+    </StyledButton>
+  );
+}
