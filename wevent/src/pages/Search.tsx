911aa5921f2d5498d61e390e7c5db82982703588
daeunleeeee <lde7953@gmail.com>
2023-11-27 02:30:40 +0900

    Feat: t상세페이지 메뉴까지

--- wevent/src/pages/Search.tsx
+++ wevent/src/pages/Search.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from 'components/Header/Header';
+import Wrapper from 'components/Container/Wrapper';
+import Footer from 'components/Footer';
+import SearchCard from 'components/card/SearchCard';
+import SectionLine from 'components/SectionLine';
+import SearchSection from 'components/card/SearchSection';
+import SearchContentsCard from 'components/card/SearchContentsCard';
 
-function Search() {
-  return <div>서치페이지</div>;
+export default function Search() {
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <SearchCard />
+
+        <SectionLine />
+        <SearchSection />
+        <SectionLine />
+
+        <SearchContentsCard />
+      </Wrapper>
+      <Footer />
+    </>
+  );
 }
-
-export default Search;
